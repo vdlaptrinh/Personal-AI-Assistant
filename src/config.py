54a773b8ed052676893c7e5b36e47867f3e768e4
vdlaptrinh <vdlaptrinh@gmail.com>
2024-12-15 18:31:40 +0700

    fix

---
+++ src/config.py
@@ -0,0 +1,7 @@
+"""
+    This is an example for config.py
+    Create config.py yourself with the configuration below with your own API keys
+"""
+
+porcupine_access_key = "******************"
+gpt_key = "YOUR_KEY"
